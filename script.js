564fea413380cc385495e986401e2a82a9f95e04
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-01-12 17:05:32 +0530

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 //console.log("Hello JS");
 
-// For each practice
+// For-each loop practice
+
+Arr = ["First", "Second","Third","Fourth","Fifth"]; // Sample Array
+
+// 0 1 2 3 4 5
+
+// Printing all elements in array
+for(let i = 0; i < Arr.length; i++){
+    console.log(Arr[i]); //Print
+}
+
+// Access (orginal array) --> print
+
+// For-each (loop)
+
+Arr.forEach(fefn);
+
+// Element Index Array
+
+function fefn(ele, ind, Arr){
+    console.log(ele.length);
+    console.log(ind);
+    console.log(Arr.length);
+}
+
+// Function (Machine (Input -> Output)) 
+// Ganne ke juice machine (Ganna -> Juice)
+
+function GanneKaJuice(ganna, Ice){
+    // Processing or Working of func
+    let juice = ganna + Ice;
+    return juice;
+
+}
+
+let glass = GanneKaJuice(7, 2);
+console.log(glass);
+
+function sdmrf(paresh,labhesh){
+    let maruf=paresh+labhesh;
+    return maruf;
+
+}
+
+let sd =sdmrf(7,2);
+console.log(sd);
