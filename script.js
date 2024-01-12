fa7691d643ecdeb33da091090ecb4ebbee0ef54c
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-01-12 17:32:05 +0530

    Updated

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
-//console.log("Hello JS");
+// //console.log("Hello JS");
 
-// For-each loop practice
+// // For-each loop practice
 
-Arr = ["First", "Second","Third","Fourth","Fifth"]; // Sample Array
+// Arr = ["First", "Second","Third","Fourth","Fifth"]; // Sample Array
 
-// 0 1 2 3 4 5
+// // 0 1 2 3 4 5
 
-// Printing all elements in array
-for(let i = 0; i < Arr.length; i++){
-    console.log(Arr[i]); //Print
-}
+// // Printing all elements in array
+// for(let i = 0; i < Arr.length; i++){
+//     console.log(Arr[i]); //Print
+// }
 
-// Access (orginal array) --> print
+// // Access (original array) --> print
 
-// For-each (loop)
+// // For-each (loop)
 
-Arr.forEach(fefn);
+// Arr.forEach(fefn);
 
-// Element Index Array
+// // Element Index Array
 
-function fefn(ele, ind, Arr){
-    console.log(ele.length);
-    console.log(ind);
-    console.log(Arr.length);
-}
+// function fefn(ele, ind, Arr){
+//     console.log(ele.length);
+//     console.log(ind);
+//     console.log(Arr.length);
+// }
 
-// Function (Machine (Input -> Output)) 
-// Ganne ke juice machine (Ganna -> Juice)
+// // Function (Machine (Input -> Output)) 
+// // Ganne ke juice machine (Ganna -> Juice)
 
-function GanneKaJuice(ganna, Ice){
-    // Processing or Working of func
-    let juice = ganna + Ice;
-    return juice;
+// function GanneKaJuice(ganna, Ice){
+//     // Processing or Working of func
+//     let juice = ganna + Ice;
+//     return juice;
 
-}
+// }
 
-let glass = GanneKaJuice(7, 2);
-console.log(glass);
+// let glass = GanneKaJuice(7, 2);
+// console.log(glass);
 
-function sdmrf(paresh,labhesh){
-    let maruf=paresh+labhesh;
-    return maruf;
+// function sdmrf(paresh,labhesh){
+//     let maruf=paresh+labhesh;
+//     return maruf;
 
-}
+// }
 
-let sd =sdmrf(7,2);
-console.log(sd);
+// let sd =sdmrf(7,2);
+// console.log(sd);
+
+Arrows = ["First", "Second","Third","Fourth","Fifth"];
+
+Arrows.forEach((Arrow, i) =>{
+
+});
+
